43f9d2dddc0b7b77355011d25085d8aabbcad844
Sgt. Alexis <sgt.alexis@gmail.com>
2023-02-24 12:56:34 +0100

    Initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## [v1.0.0 (2023-02-24)](https://github.com/SgtAlexis/SgtBain/releases/tag/v1.0.0)
+Initial release
---
+++ SgtBain/lua/missionscriptelement.lua
@@ -0,0 +1,23 @@
+local function on_executed(name)
+	if not Utils:IsInHeist() then
+		return
+	end
+	SgtBain:Log("on_executed(" .. name .. ")")
+	if SgtBain.messages[name] then
+		local message = SgtBain.messages[name]
+		local count = (SgtBain.counts[name] or 0) + 1
+		local limit = SgtBain.limits[name] and SgtBain.limits[name][Global.game_settings.level_id] or 1
+		if limit == count then
+			managers.chat:send_message(ChatManager.GAME, nil, message)
+		end
+		SgtBain.counts[name] = count
+	end
+end
+
+Hooks:PostHook(MissionScriptElement, "on_executed", SgtBain:HookName("MissionScriptElement", "on_executed"), function(self)
+	on_executed(self._editor_name)
+end)
+
+Hooks:PostHook(MissionScriptElement, "client_on_executed", SgtBain:HookName("MissionScriptElement", "client_on_executed"), function(self)
+	on_executed(self._editor_name)
+end)
